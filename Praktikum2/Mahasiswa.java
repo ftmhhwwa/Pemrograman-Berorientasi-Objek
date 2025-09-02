295e686326f520620051338f5fd5c7eb108a26f5
ftmhhwwa <fatimahhawwa0201@gmail.com>
2025-09-02 16:40:07 +0700

    Praktikum 2
    class, object, dan encapsulation

---
+++ Praktikum2/Mahasiswa.java
@@ -0,0 +1,29 @@
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String jurusan;
+
+    // Constructor
+    public Mahasiswa(String nama, String nim, String jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+
+    // Getter & Setter
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void infoMahasiswa() {
+        System.out.println("Nama: " + nama + "  \nNIM: " + nim + "  \nJurusan: " + jurusan);
+    }
+}
---
+++ Praktikum2/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+/**
+ * Main class untuk menjalankan aplikasi peminjaman ruang kuliah.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Daftar Mahasiswa
+        Mahasiswa[] daftarMahasiswa = {
+            new Mahasiswa("Fatimah Hawwa", "241511074", "Teknik Informatika"),
+            new Mahasiswa("Renjun", "241511100", "Teknik Informatika"),
+            new Mahasiswa("Mark", "241511101", "Bahasa Inggris")
+        };
+
+        // Daftar Ruang
+        RuangKelas[] daftarRuang = {
+            new RuangKelas("D-101", "Gedung JTK", 40),
+            new RuangKelas("D-112", "Gedung JTK", 35),
+            new RuangKelas("D-116", "Gedung JTK", 50)
+        };
+
+        int pilihan;
+        do {
+            System.out.println("\n=== Daftar Mahasiswa ===");
+            for (int i = 0; i < daftarMahasiswa.length; i++) {
+                System.out.print((i + 1) + ". ");
+                daftarMahasiswa[i].infoMahasiswa();
+            }
+
+            System.out.println("\n=== Daftar Ruangan ===");
+            for (int i = 0; i < daftarRuang.length; i++) {
+                System.out.print((i + 1) + ". ");
+                daftarRuang[i].infoRuang();
+            }
+
+            // Menu utama
+            System.out.println("\nMenu:");
+            System.out.println("1. Pinjam Ruangan");
+            System.out.println("2. Kosongkan Ruangan");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih: ");
+            pilihan = input.nextInt();
+
+            switch (pilihan) {
+                case 1 -> {
+                    System.out.print("Pilih mahasiswa (nomor): ");
+                    int pilihMhs = input.nextInt() - 1;
+                    System.out.print("Pilih ruangan yang ingin dipinjam (nomor): ");
+                    int pilihRuang = input.nextInt() - 1;
+
+                    if (pilihMhs >= 0 && pilihMhs < daftarMahasiswa.length &&
+                        pilihRuang >= 0 && pilihRuang < daftarRuang.length) {
+
+                        if (!daftarRuang[pilihRuang].isTerpakai()) {
+                            daftarRuang[pilihRuang].pinjamRuang();
+                            System.out.println("|Success|\n" + daftarMahasiswa[pilihMhs].getNama() +
+                                               " berhasil meminjam ruangan " +
+                                               daftarRuang[pilihRuang].getKodeRuang());
+                        } else {
+                            System.out.println("|Failed|\nRuangan " + daftarRuang[pilihRuang].getKodeRuang() + " sedang terpakai.");
+                        }
+                    } else {
+                        System.out.println("Pilihan tidak valid.");
+                    }
+                }
+                case 2 -> {
+                    System.out.print("Pilih ruangan yang ingin dikosongkan (nomor): ");
+                    int pilihKosong = input.nextInt() - 1;
+
+                    if (pilihKosong >= 0 && pilihKosong < daftarRuang.length) {
+                        daftarRuang[pilihKosong].kosongkanRuang();
+                        System.out.println("|Success|\nRuangan berhasil dikosongkan.");
+                    } else {
+                        System.out.println("Pilihan tidak valid.");
+                    }
+                }
+                case 0 -> System.out.println("Program selesai. Terima kasih!");
+                default -> System.out.println("Pilihan tidak valid.");
+            }
+        } while (pilihan != 0);
+
+        input.close();
+    }
+}
